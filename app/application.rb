d44fd552f3e01a003fdfeb73fa0c049b9738bfb5
BarretteBanner <barrettebanner@gmail.com>
2019-08-28 10:40:44 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,8 @@
 class Application 
     def call(env)
         resp = Rack::Response.new
-        Now = Time.now
-        if Now.hour <= 12
+        now = Time.now
+        if now.hour <= 12
             resp.write "Good Morning"
         else 
             resp.write "Good Afternoon"
